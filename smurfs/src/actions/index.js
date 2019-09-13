df3685207d8875cc1076ea3fcfb00d39fd628d0c
Taran <thetaranneeld@gmail.com>
2019-09-13 10:24:45 -0600

    added post request

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,17 +1,24 @@
 import axios from "axios";
+
 export const GET_SMURFS = "GET_SMURFS";
-export const ADD_SMURFS = "ADD_SMURFS";
 export const GET_SMURFS_FAILED = "GET_SMURFS_FAILED";
+export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => dispatch => {
     dispatch({ type: GET_SMURFS });
     axios
-        .get("/smurfs")
+        .get(`http://localhost:3333/smurfs`)
         .then(res => {
-          console.log(res)
-            dispatch({ type: GET_SMURFS, payload: res.data.message });
+            dispatch({ type: GET_SMURFS, payload: res.data });
         })
         .catch(err => {
             dispatch({ type: GET_SMURFS_FAILED, payload: err });
         });
 };
+
+export const addSmurf = (dispatch, smurf) => {
+    return axios.post('http://localhost:3333/smurfs', smurf)
+        .then(res => {
+            dispatch({ type: ADD_SMURF, payload: res.data });
+        });
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,6 @@
 import {
   GET_SMURFS,
-  ADD_SMURFS,
+  ADD_SMURF,
   GET_SMURFS_FAILED
 } from "../actions";
 
@@ -16,19 +16,20 @@ export const rootReducer = (state = initialState, action) => {
     case GET_SMURFS:
       return {
         ...state,
-        gettingSmurfs: true,
-        error: ''
-      }
-    case ADD_SMURFS:
-      return {
-        ...state,
         smurfs: [...state.smurfs, action.payload],
-        gettingSmurfs: false,
+        gettingSmurfs: true,
         error: ''
       }
     case GET_SMURFS_FAILED:
       return {
         error: action.payload
+      }
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, action.payload],
+        gettingSmurfs: false,
+        error: ''
       }
     default:
       return state
